a2852014b0fdf473a241db15053bb577e5dd891c
Vanessa Lane <vlane@23andme.com>
2020-09-25 16:48:40 -1000

    add email validation

--- models/User.js
+++ models/User.js
@@ -11,7 +11,10 @@ const UserSchema = new Schema({
         type: String,
         required: true,
         trim: true,
-        unique: true
+        validate: function validateEmail(email) {
+            const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return regex.test(String(email).toLowerCase());
+        }
     },
     thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
     friends: [{ type: Schema.Types.ObjectId, ref: 'User' }]
